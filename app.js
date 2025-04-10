87275896550ac5324b141449832e4d44601028dd
siddu581 <siddusiddendra@gmail.com>
2025-04-10 12:20:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ function calculate(){
     const monthDiff = present.getMonth() -birthDate.getMonth();
 
     if(monthDiff < 0  || (monthDiff === 0 && present.getDate() << birthDate.getDate())){
-        document.getElementById('rst').innerText = `Your are entering invalid year`
-    }  else{  
+        age--
+    }  
     document.getElementById('rst').innerText = `Your age is ${age} years`
-    }
+   
 }
